a075f8b13fc8e37822bde206503aa04bfb3a4670
Praveen-Annavarapu <100507731+Praveen-Annavarapu@users.noreply.github.com>
2022-03-27 22:16:21 +0530

    Boolean

---
+++ Boolean.java
@@ -0,0 +1,11 @@
+public class Boolean {
+
+    public static void main(String[] args) {
+        // BOLEAN = true (or) false
+        boolean b = true;
+        boolean a = !b;
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}
