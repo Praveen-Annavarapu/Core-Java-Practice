8044e661e42776dda1e2864e2cd9326253b8e1af
Praveen-Annavarapu <100507731+Praveen-Annavarapu@users.noreply.github.com>
2022-03-27 22:42:16 +0530

    Create No ObjectOfClass.java

---
+++ No ObjectOfClass.java
@@ -0,0 +1,16 @@
+public class NoObjectOfClass {
+    static int i;
+    public static void main(String[] arg)
+    {
+        NoObjectOfClass obj1 = new NoObjectOfClass();
+        NoObjectOfClass obj2 = new NoObjectOfClass();
+        NoObjectOfClass obj3 = new NoObjectOfClass();
+        NoObjectOfClass obj4 = new NoObjectOfClass();
+        System.out.println(i);
+    }
+
+    NoObjectOfClass(){
+        i++ ;
+    }
+    
+}
